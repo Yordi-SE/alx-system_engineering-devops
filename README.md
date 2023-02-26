8b2ef2af3ac144d425b2c7df8498e06cd7feec33
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-26 08:51:28 -0800

    web infrastructure

---
+++ README.md
@@ -0,0 +1,1 @@
+alx SE system engineering devops
---
+++ c_tutor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	char m,p,r;
+	int age;
+	printf("hello!, please inter your fucking initials followed by your damn age");
+	scanf("%c%c%c %d",&m,&p,&r,&age);
+	printf("my initials are %c antu %c way gud %c and my age is: %d.\n",m,p,r,age);
+	return(0);
+}
